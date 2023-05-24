104223ef4f89642974d27967b6bac089cd5ef8c8
stranavadavid <stranavadavid@protonmail.com>
2023-05-24 21:15:48 +0200

    added protection

--- server/api/score.put.ts
+++ server/api/score.put.ts
@@ -30,6 +30,7 @@ export default defineEventHandler(async(event) => {
         wrong
     } = data.data;
 
+    // Wrong can be negative
     const score = correct ? 10 : Math.abs(wrong) * -2;
 
     const {data: dbData}: {data: {score: number}} = await client.from('score').select('score').eq('user_id', userId).single();
